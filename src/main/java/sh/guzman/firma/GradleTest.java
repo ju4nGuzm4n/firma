d109f6def9890f22f0ec31d6d1c3d83c1128326b
juan <ivan.valencia@proveedoresgnp.mx>
2023-01-05 15:14:47 -0600

    subida repo

---
+++ src/main/java/sh/guzman/firma/GradleTest.java
@@ -0,0 +1,13 @@
+package sh.guzman.firma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTest.class, args);
+	}
+
+}
